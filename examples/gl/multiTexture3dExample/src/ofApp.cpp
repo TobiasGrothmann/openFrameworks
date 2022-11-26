98a9657b767d5807bad4046217c21cc7c56486a4
Tobias Grothmann <Tobias.Grothmann@googlemail.com>
2022-11-26 15:25:15 +0100

    remove extra space all templates' mouseMoved

--- examples/gl/multiTexture3dExample/src/ofApp.cpp
+++ examples/gl/multiTexture3dExample/src/ofApp.cpp
@@ -239,7 +239,7 @@ void ofApp::keyReleased(int key){
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y){
 
 }
 
